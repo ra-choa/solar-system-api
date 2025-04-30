c6456552fbd936046fe95eaa2e4dd9ba804ffaf8
Solhee <ellen.solhee.jin@gmail.com>
2025-04-30 10:00:51 -0700

    Added in < and > for filtering with query param 'moons'!'

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,7 +1,7 @@
 from flask import abort, Blueprint, make_response, request, Response
 from app.models.planet import Planet
 from ..db import db
-from sqlalchemy import cast, String
+from sqlalchemy import cast, String, Integer
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix = "/planets")
 
@@ -38,8 +38,15 @@ def get_all_planets():
     
     moons_param = request.args.get("moons")
     if moons_param: 
-        # Can try adding in more filters another time (<, >, etc.)
-        query = query.where(cast(Planet.moons, String).like(f"%{moons_param}%")) # check this later
+        # Assume that if < or > in moons_param, then it's followed by a number.
+        if '<' in moons_param:
+            num = int(moons_param[1:])
+            query = query.where(Planet.moons < num)
+        elif '>' in moons_param:
+            num = int(moons_param[1:])
+            query = query.where(Planet.moons > num)
+        else:
+            query = query.filter(Planet.moons == int(moons_param))
 
     query = query.order_by(Planet.id)
     planets = db.session.scalars(query)
@@ -100,39 +107,3 @@ def delete_planet(planet_id):
     db.session.commit()
 
     return Response(status=204, mimetype="application/json")
-
-# @planets_bp.get("")
-# def get_all_planets():
-#     planets_response = []
-#     for planet in planets:
-#         planets_response.append(
-#             {"id": planet.id,
-#             "name": planet.name,
-#             "description": planet.description,
-#             "moon": planet.description}
-#         )
-#     return planets_response
-
-# def validate_planet(id):
-#     try:
-#         id = int(id)
-#     except:
-#         response = {"message": f"planet {id} invalid"}
-#         abort(make_response(response, 400))
-
-#     for planet in planets:
-#         if planet.id == id:
-#             return planet
-    
-#     response = {"message": f"planet {id} not found"}
-#     abort(make_response(response, 404))
-
-# @planets_bp.get("/<id>")
-# def get_one_planet(id):
-#     planet = validate_planet(id)
-#     return {
-#         "id": planet.id,
-#         "name": planet.name,
-#             "description": planet.description,
-#             "moon": planet.description
-#     }
